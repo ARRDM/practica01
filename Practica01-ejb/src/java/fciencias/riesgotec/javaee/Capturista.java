4cba7913ba6e99a9aa6296ea4ef532270ce3c16e
ricardorodab <ricardorodab19@gmail.com>
2016-02-27 17:09:09 -0600

    cascarones de modelos

--- Practica01-ejb/src/java/fciencias/riesgotec/javaee/Capturista.java
+++ Practica01-ejb/src/java/fciencias/riesgotec/javaee/Capturista.java
@@ -8,40 +8,126 @@ package fciencias.riesgotec.javaee;
 import java.io.IOException;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.context.FacesContext;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  *
  * @author ricardo_rodab
  */
 @ApplicationScoped
+@Entity
+@Table(name="capturista")
 public class Capturista {
     
-    private int contador;
-    private String aManipular;
-    
-    
-    public void setContador(int contador) {
-        this.contador = contador;
+    /** Llave primaria de la tabla capturista. */
+    private int id;
+    /** Nombre de capturista. */
+    private String nombre;
+    /** Apellido paterno del  capturista. */
+    private String apellidoPaterno;
+    /** Apellido materno del capturista. */
+    private String apellidoMaterno;
+
+    /**
+     * Metodo constructor para un nuevo capturista.
+     * @param id - Es el id del capturista.
+     * @param nombre - Su nombre.
+     * @param apellidoPaterno - Su apellido paterno.
+     * @param apellidoMaterno - Su apellido materno.
+     */
+    public Capturista(int id, String nombre, String apellidoPaterno, String apellidoMaterno) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
     }
 
-    public void setAManipular(String aManipular) {
-        this.aManipular = aManipular;
+        /**
+     * Metodo constructor sin apellido materno para un nuevo capturista.
+     * @param id - Es el id del capturista.
+     * @param nombre - Su nombre.
+     * @param apellidoPaterno - Su apellido paterno.
+     */
+    public Capturista(int id, String nombre, String apellidoPaterno) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
     }
 
-    public int getContador() {
-        return contador;
+    /**
+     * Metodo constructor que se ofrece por completud.
+     */
+    public Capturista(){}
+    
+    
+    
+    /**
+     * Obtiene el id del capturista.
+     * @return El id del objeto.
+     */
+    @Id
+    public int getId() {
+        return id;
+    }
+    
+    /**
+     * Sustituye el id por uno nuevo.
+     * @param id - El nuevo id.
+     */
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public String getAManipular() {
-        return aManipular;
+    /**
+     * Obtiene el nombre del capturista.
+     * @return El nombre del capturista.
+     */
+    public String getNombre() {
+        return nombre;
     }
     
-    
-    public void atiende(String cadena)throws IOException{
-        this.contador++;
-        this.aManipular = cadena.toUpperCase();
-        FacesContext.getCurrentInstance().getExternalContext()
-                .redirect("index.xhtml");
+    /**
+     * Cambia el nombre del capturista.
+     * @param nombre - Es el nuevo nombre del capturista.
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
-    
-}
+
+    /**
+     * Nos da el apellido parteno del capturista.
+     * @return El apellido paterno.
+     */
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    /**
+     * Nos da el apellido materno si tiene, de lo contrario regresa la cadena
+     * vacía.
+     * @return El apellido materno o "". 
+     */
+    public String getApellidoMaterno() {
+        if(apellidoMaterno == null)
+            return "";
+        return apellidoMaterno;
+    }
+
+    /**
+     * Asigna un nuevo apellido paterno al capturista.
+     * @param apellidoPaterno - Es el nuevo apellido.
+     */
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    /**
+     * Asigna un nuevo apellido materno al capturista.
+     * @param apellidoMaterno - Es el nuevo apellido.
+     */
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }  
+} //Fin de Capturista.java
--- Practica01-ejb/src/java/fciencias/riesgotec/javaee/Venta.java
+++ Practica01-ejb/src/java/fciencias/riesgotec/javaee/Venta.java
@@ -5,10 +5,131 @@
  */
 package fciencias.riesgotec.javaee;
 
+import javax.faces.bean.ApplicationScoped;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author ricardo_rodab
  */
+@ApplicationScoped
+@Entity
+@Table(name="venta")
 public class Venta {
+      
+    /** Llave primaria de la tabla venta. */
+    private int id;
+    /** Anio de la venta. */
+    private int anio;
+    /** Mes de la venta. */
+    private int mes;
+    /** Dia de la venta. */
+    private int dia;
+    /** Suma total de la venta. */
+    private double total;
+
+    /**
+     * Constructor de la clase Venta.
+     * @param id - El identificador primario.
+     * @param anio - El año de la venta.
+     * @param mes´- El mes de la venta.
+     * @param dia - El día de la venta.
+     * @param total - La suma total de lo que se vendió.
+     */
+    public Venta(int id, int anio, int mes, int dia, double total) {
+        this.id = id;
+        this.anio = anio;
+        this.mes = mes;
+        this.dia = dia;
+        this.total = total;
+    }
     
+    /**
+     * Constructor que se ofrece por completud.
+     */
+    public Venta(){}
+
+    /**
+     * Regresa el identificador primario del objeto.
+     * @return el id del objeto.
+     */
+    @Id
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Nos regresa el año de la venta.
+     * @return El año.
+     */
+    public int getAnio() {
+        return anio;
+    }
+
+    /**
+     * Nos regresa el mes de la venta.
+     * @return El mes.
+     */
+    public int getMes() {
+        return mes;
+    }
+
+    /**
+     * Nos regresa el día de la venta.
+     * @return El día.
+     */
+    public int getDia() {
+        return dia;
+    }
+
+    /**
+     * Nos regresa la suma total de lo que se vendió.
+     * @return El total.
+     */
+    public double getTotal() {
+        return total;
+    }
+
+    /**
+     * Asigna un nuevo id al objeto.
+     * @param id - El nuevo id.
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Asigna un nuevo año al objeto.
+     * @param anio El año.
+     */
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    /**
+     * Asigna un nuevo mes al objeto.
+     * @param mes - El nuevo mes.
+     */
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    /**
+     * Asigna un nuevo día al objeto.
+     * @param dia - El nuevo día.
+     */
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    /**
+     * Asigna el total de venta al objeto.
+     * @param total - El total de venta.
+     */
+    public void setTotal(double total) {
+        this.total = total;
+    }
+   
 }
